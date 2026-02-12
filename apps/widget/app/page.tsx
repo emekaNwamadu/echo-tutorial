030dc8b8b717e3c00cd09a10e6268f2e36070028
Emekanwamadu <emekanwamadu@outlook.com>
2026-02-12 22:25:06 +0100

    feat: add turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,11 @@
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello web/app </h1>
-        <Button size="sm">Button </Button>
-        <p>{add(2, 5)}</p>
-        <Input />
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
+        <Button size="sm">Button</Button>
       </div>
     </div>
   );
