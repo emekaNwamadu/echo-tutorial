38cfed9a5d526639b08278cc69f138c213c381f7
Emekanwamadu <emekanwamadu@outlook.com>
2026-02-17 15:32:11 +0100

    04:organizations

---
+++ apps/web/app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { AuthGuard } from "@/modules/auth/ui/components/authguard";
+import { OrganizationGuard } from "@/modules/auth/ui/components/organisation-guard";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AuthGuard>
+      <OrganizationGuard>{children}</OrganizationGuard>
+    </AuthGuard>
+  );
+};
+
+export default Layout;
