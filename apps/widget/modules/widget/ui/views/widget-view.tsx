c440c01e3e2ae0d4fc35e66c902b6ab7e25f1138
Emekanwamadu <emekanwamadu@outlook.com>
2026-03-25 21:14:39 +0100

    11: widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-// import { WidgetFooter } from "../components/widget-footer";
-// import { WidgetHeader } from "../components/widget-header";
+import { useAtomValue } from "jotai";
 
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "../../atoms/widget-atoms";
+
 interface Props {
   organizationId: string;
 }
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+  const screenComponents = {
+    error: <p>TODO: Error </p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+    main: <p>TODO: Main</p>,
+  };
+
   return (
     //TODO: confirm min-h-screen min-w-screen is needed here, or if we can just use h-full w-full
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   );
 };
